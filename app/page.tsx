4f32ba62e7967b019ae3e8ae226a96f5bf8dd687
hansolo228-dev <arthur@getblock.io>
2023-10-09 14:40:28 +0300

    update connection

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import { ethers } from "ethers"
 import Getblock from '@/contracts/Storage.abi.json'
 import { Button, Flex, useColorModeValue } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
+import { CheckCircleIcon } from '@chakra-ui/icons'
 
 
 export default function Home() {
@@ -21,19 +23,29 @@ export default function Home() {
   };
   return (
     <Flex h="100vh" justifyContent="center" alignItems="center" bgColor={`#00B5D8`}>
-      <Button
-        px={8}
-        bg={useColorModeValue('#151f21', 'gray.900')}
-        color={'white'}
-        rounded={'md'}
-        _hover={{
-          transform: 'translateY(-2px)',
-          boxShadow: 'lg',
-        }}
-        onClick={deploy}>
-        Click Me
-      </Button>
-      {contractAddress}
+      {contractAddress === "" && (
+        <Button
+          px={8}
+          bg={useColorModeValue('#151f21', 'gray.900')}
+          color={'white'}
+          rounded={'md'}
+          _hover={{
+            transform: 'translateY(-2px)',
+            boxShadow: 'lg',
+          }}
+          onClick={deploy}>
+          Click Me
+        </Button>
+      )}
+      {contractAddress !== "" && (
+        <Box textAlign="center" py={10} px={6}>
+          <CheckCircleIcon boxSize={'50px'} color={'green.500'} />
+          <Heading as="h2" size="xl" mt={6} mb={2}>
+            {contractAddress}
+          </Heading>
+        </Box>
+
+      )}
     </Flex>
   )
 }
---
+++ components/Result.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Box, Heading } from '@chakra-ui/react'
+import { CheckCircleIcon } from '@chakra-ui/icons'
+
+export default function Success(contract: any) {
+  return (
+    <>  
+</>
+  )
+}
